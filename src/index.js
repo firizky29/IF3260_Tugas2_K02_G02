f308d51719d1de459dcb3ea17ab35423425d26b7
leocardhio <leocardhio@gmail.com>
2023-03-25 14:34:19 +0700

    [refactor] initial radius value

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ let state = {
   fudgeFactor: 0,
   obliqueTetha: 0,
   obliquePhi: 0,
-  cameraRadius: 1.3,
+  cameraRadius: -1.3,
   cameraRotation: Converter.degToRad(0),
 };
 const renderSettings = {
@@ -61,22 +61,20 @@ const eventHandler = {
           return;
         }
 
-        
         var reader = new FileReader();
         reader.readAsText(file, 'UTF-8');
-        
+
         reader.onload = (readerEvent) => {
           var content = readerEvent.target.result;
           content = JSON.parse(content);
           // set all to default
-          state.model = content     
+          state.model = content;
           renderSettings.drawCounter = content.vertices.length;
-          const toDefault = document.querySelector('button#toDefault')
+          const toDefault = document.querySelector('button#toDefault');
           toDefault.click();
 
-          webgl2 = new WebGL2Handler(document.querySelector('canvas')).init()
-            
-            
+          webgl2 = new WebGL2Handler(document.querySelector('canvas')).init();
+
           webgl2
             .clearBuffer()
             .setVertices(state.model.vertices)
@@ -147,7 +145,7 @@ const eventHandler = {
         fudgeFactor: 0,
         obliqueTetha: 0,
         obliquePhi: 0,
-        cameraRadius: 1.3,
+        cameraRadius: -1.3,
         cameraRotation: Converter.degToRad(0),
       };
       document.querySelector('#projection').value = initialState.projectionType;
@@ -165,7 +163,7 @@ const eventHandler = {
         .querySelectorAll('.scaling')
         .forEach((el, idx) => (el.value = initialState.scale[idx]));
 
-      state = { ...initialState, model: state.model}
+      state = { ...initialState, model: state.model };
       webgl2.clearBuffer().render(renderSettings, state);
     };
   },
@@ -268,6 +266,5 @@ document.getElementById('save').addEventListener('click', saveToJSON);
 
 const startAnimation = () => {
   webgl2.clearBuffer().renderAnimation(renderSettings, state);
-}
+};
 document.getElementById('animate').addEventListener('click', startAnimation);
-
